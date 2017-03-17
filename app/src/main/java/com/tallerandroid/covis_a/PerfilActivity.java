10814881bedb176bff5eb83eef8c3fcc69626387
eric_ <eric>
2017-03-17 09:38:07 -0500

    perfil

---
+++ app/src/main/java/com/tallerandroid/covis_a/PerfilActivity.java
@@ -0,0 +1,17 @@
+package com.tallerandroid.covis_a;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eric_ on 17/03/2017.
+ */
+
+public class PerfilActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil);
+    }
+}
