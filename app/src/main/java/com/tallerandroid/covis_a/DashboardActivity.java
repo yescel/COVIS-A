bd1473d21d85b79ab50dc557434c1977664cc354
Yessica Celina Valdes Farias <yescel06@gmail.com>
2017-03-17 10:22:30 -0500

    Merge branch 'master' of https://github.com/yescel/COVIS-A
    # Conflicts:
    #	app/src/main/res/layout/perfil.xml
    #	app/src/main/res/mipmap-xxxhdpi/icono-de-usuario-masculino_17-810120247.jpg
    #	app/src/main/res/mipmap-xxxhdpi/userman.jpg
    #	app/src/main/res/mipmap-xxxhdpi/usuariosman.jpg

--- app/src/main/java/com/tallerandroid/covis_a/DashboardActivity.java
+++ app/src/main/java/com/tallerandroid/covis_a/DashboardActivity.java
@@ -1,8 +1,10 @@
 package com.tallerandroid.covis_a;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -41,5 +43,20 @@ public class DashboardActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
 
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();//obtenemos el id del item que le dimos clic en la barra de menu
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.Perfil) { //podemos hacer un switch o varios activity
+            Intent intent = new Intent(DashboardActivity.this, PerfilActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
