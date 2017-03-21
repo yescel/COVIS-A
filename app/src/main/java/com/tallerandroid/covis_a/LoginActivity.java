ff3f041a1924672bc7244b81d22582a844e0a165
scarletttth <scarleth_h@yahoo.com>
2017-03-20 19:44:55 -0500

    Modificacion del login

--- app/src/main/java/com/tallerandroid/covis_a/LoginActivity.java
+++ app/src/main/java/com/tallerandroid/covis_a/LoginActivity.java
@@ -1,37 +1,144 @@
 package com.tallerandroid.covis_a;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button intent;
-    TextView intent2;
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @InjectView(R.id.txtEmail)
+    EditText _emailText;
+    @InjectView(R.id.txtPassword)
+    EditText _passwordText;
+    @InjectView(R.id.btnRegistrar)
+    Button _loginButton;
+    @InjectView(R.id.link_signup)
+    TextView _signupLink;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        intent = (Button) findViewById(R.id.btnRegistrar);
-        intent.setOnClickListener(new View.OnClickListener() {
+        //
+        ButterKnife.inject(this);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                startActivity(intent);
+                login();
+
             }
         });
 
-        intent2 = (TextView) findViewById(R.id.link_signup);
-        intent2.setOnClickListener(new View.OnClickListener() {
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent2);
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
             }
         });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.Theme_AppCompat_DayNight_Dialog_Alert);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Autentificando...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implementar la lógica de registro exitosa aquí
+                // De forma predeterminada, acabamos de terminar la Actividad y registrarlos automáticamente
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Desactivar volver a la MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+        startActivityForResult(intent, REQUEST_SIGNUP);
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "El inicio de sesión falló!", Toast.LENGTH_LONG).show();
+        _loginButton.setEnabled(true);
+    }
+
+    //Validación de email y password
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("Ingrese un correo válido");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("Entre 4 a 10 carácteres alfanuméricos");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
     }
 }
