185c49e2b8597b27bf58280ffc68093b9de2c78e
eric_ <eric>
2017-03-23 02:09:22 -0500

    z

--- app/src/main/java/com/tallerandroid/covis_a/Perfil.java
+++ app/src/main/java/com/tallerandroid/covis_a/Perfil.java
@@ -1,16 +1,13 @@
 package com.tallerandroid.covis_a;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-/**
- * Created by eric_ on 17/03/2017.
- */
+public class Perfil extends AppCompatActivity {
 
-public class PerfilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.perfil);
+        setContentView(R.layout.activity_perfil);
     }
 }
